220e4cbd7c94b1f131fa4b29f03c1b72ea3f2e63
Cesar <cesarcorregiari@gmail.com>
2022-10-02 18:09:26 -0300

    Create constants file

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+// HOME
+export const LOCATION = 'Toronto, Canada'
+export const PLEASE_FILL_FORM = 'Please fill out the following form to join the Pipeline'
+export const VIEW_JOB_DESCRIPTION = 'View Job Description'
+export const PIPELINE = 'This pipeline belongs to'
+
+// FORM
+export const UPLOAD_RESUME = 'Upload Resume (Optional)'
+export const FILE_TYPE = 'PDF or Word Document (.doc, .docx), smaller than 5 MB'
