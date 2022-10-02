dfeb8b4c673dcdf3676df210b82d5187a91dcbc2
Cesar <cesarcorregiari@gmail.com>
2022-10-02 17:58:20 -0300

    Add mock data object

---
+++ pages/api/dataJS.js
@@ -0,0 +1,19 @@
+export const dataJS = {
+  jobLinkId: 1451,
+  code: "4KGQ5SRD",
+  jobTitle: "GraphStax",
+  description: "This is a sample description for GrtaphStax",
+  inviteTitle: null,
+  inviteMessage: null,
+  companyCountry: 'CAN',
+  companyCity: 'Toronto',
+  companyProvince: 'ON',
+  workModel: 'HYBRID',
+  companyOrName: 'Workwolf',
+  active: true,
+  contentType: null,
+  fileName: null,
+  brand: null,
+  careerFitScore: null,
+  incompleteScreenerQuestions: null
+}
